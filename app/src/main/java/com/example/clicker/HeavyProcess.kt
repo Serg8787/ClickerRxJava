4cd2afd91b3c28b3d128175d17b08e66c34d4a1a
Serg8787 <tsybulnikserg@gmail.com>
2021-08-11 13:18:33 +0300

    add new obserbal

--- app/src/main/java/com/example/clicker/HeavyProcess.kt
+++ app/src/main/java/com/example/clicker/HeavyProcess.kt
@@ -3,35 +3,32 @@ package com.example.clicker
 import android.os.SystemClock
 import android.util.Log
 import io.reactivex.Observable
-import io.reactivex.Observer
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.disposables.Disposable
 import io.reactivex.internal.operators.observable.ObservableCreate
-import io.reactivex.schedulers.Schedulers
-import kotlinx.android.synthetic.main.fragment_new_competion.*
 
 class HeavyProcess {
 
-    fun getStrTimer(): Observable<String> {
-        return ObservableCreate ({ subcrumber ->
-            val s = arrayListOf ("3","2","1","Start",
-                "00:10",
-                "00:09",
-                "00:08",
-                "00:07",
-                "00:06",
-                "00:05",
-                "00:04",
-                "00:03",
-                "00:02",
-                "00:01",
-                "00:00",
-            )
-            for ((i,value ) in s.withIndex()) {
+    fun getThreeToStart(): Observable<String> {
+        return ObservableCreate { subcrumber ->
+            val s = arrayListOf("3", "2", "1", "Start")
+            for ((i, value) in s.withIndex()) {
                 subcrumber.onNext(value)
                 SystemClock.sleep(1000)
             }
-        })
+        }
+    }
+
+    fun getSeconds(seconds: Int): Observable<Int> {
+        return ObservableCreate { subcrumber ->
+            SystemClock.sleep(3000)
+            for (i in seconds downTo 0) {
+                subcrumber.onNext(i)
+                SystemClock.sleep(1000)
+            }
+        }
+    }
+
+    fun stop() {
+        HeavyProcess().stop()
     }
 
 }
--- app/src/main/java/com/example/clicker/PlayGameFragment.kt
+++ app/src/main/java/com/example/clicker/PlayGameFragment.kt
@@ -1,13 +1,10 @@
 package com.example.clicker
 
 import android.annotation.SuppressLint
-import android.content.Context
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
@@ -32,70 +29,23 @@ class PlayGameFragment : Fragment() {
         val countLevel: Int = requireArguments().get("countLevel").toString().toInt()
         var countResult: Int = 0
 
-        HeavyProcess().getStrTimer().subscribeOn(Schedulers.newThread())
-            .observeOn(AndroidSchedulers.mainThread()).subscribe ({
-                tvStartTimer.text = it
-                if (it.equals("00:10")) {
-                    tvStartTimer.background = resources.getDrawable(R.drawable.back_record_long)
-                    clRoot.setOnClickListener {
-                        countResult++
-                        Log.d("MyLog", countResult.toString())
-                        if (countLevel == 10) {
-                            val sharedPreferences =
-                                context?.getSharedPreferences("sharedSprint", Context.MODE_PRIVATE)
-                            val editor = sharedPreferences?.edit()
-                            editor.apply {
-                                this!!.putInt("resultSprint", countResult)
-                            }?.apply()
-                        } else if (countLevel == 20) {
-                            val sharedPreferences =
-                                context?.getSharedPreferences("sharedMiddle", Context.MODE_PRIVATE)
-                            val editor = sharedPreferences?.edit()
-                            editor.apply {
-                                this!!.putInt("resultMiddle", countResult)
-                            }?.apply()
-                        } else {
-                            val sharedPreferences =
-                                context?.getSharedPreferences("sharedLong", Context.MODE_PRIVATE)
-                            val editor = sharedPreferences?.edit()
-                            editor.apply {
-                                this!!.putInt("resultLong", countResult)
-                            }?.apply()
-                        }
-                    }
+        HeavyProcess().getThreeToStart().subscribeOn(Schedulers.newThread())
+            .observeOn(AndroidSchedulers.mainThread()).subscribe({
+                tvThreeToStart.text = it
+                if (tvThreeToStart.text.equals("Start")) {
+                    tvThreeToStart.visibility = View.GONE
                 }
-                if (it.equals("00:00")) {
-                    clRoot.isEnabled = false
-                    tvStartTimer.visibility = View.GONE
-                    tvResult.visibility = View.VISIBLE
-                    tvResult.text = "Your\nresult\n${countResult}"
-                    if(countLevel==10){
-                        val prefsSprint = context?.getSharedPreferences("sharedSprintNew", Context.MODE_PRIVATE)
-                        val resultSprintNew: Int? = prefsSprint?.getInt("resultSprintNew", 0)
-                        if(countResult> resultSprintNew!!){
-                            tvNewRecord.visibility = View.VISIBLE
-                        }
-                    }
-                    if(countLevel==20) {
-                        val prefsSprint =
-                            context?.getSharedPreferences("sharedMiddleNew", Context.MODE_PRIVATE)
-                        val resultMiddleNew: Int? = prefsSprint?.getInt("resultMiddleNew", 0)
-                        if (countResult > resultMiddleNew!!) {
-                            tvNewRecord.visibility = View.VISIBLE
-                        }
-                    }
-                    if(countLevel==30){
-                        val prefsSprint =
-                            context?.getSharedPreferences("sharedLongNew", Context.MODE_PRIVATE)
-                        val resultLongNew: Int? = prefsSprint?.getInt("resultLongNew", 0)
-                        if (countResult > resultLongNew!!) {
-                            tvNewRecord.visibility = View.VISIBLE
-                        }
-                    }
-                }
-            },{
-                Log.d("MyLog","Mistake")
             })
 
+        HeavyProcess().getSeconds(countLevel).subscribeOn(Schedulers.newThread())
+            .observeOn(AndroidSchedulers.mainThread()).subscribe({
+                tvTimer.visibility = View.VISIBLE
+                    if (it < 10) {
+                        tvTimer.text = "00 : 0$it"
+                    } else {
+                        tvTimer.text = "00 : $it"
+                    }
+
+            })
     }
 }
