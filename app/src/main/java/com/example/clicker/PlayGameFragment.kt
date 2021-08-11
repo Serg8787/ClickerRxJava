512e418ad75617bb8146135629f77e1040b68e1f
Serg8787 <tsybulnikserg@gmail.com>
2021-08-11 15:11:26 +0300

    add new obserbal with count

--- app/src/main/java/com/example/clicker/PlayGameFragment.kt
+++ app/src/main/java/com/example/clicker/PlayGameFragment.kt
@@ -46,38 +46,74 @@ class PlayGameFragment : Fragment() {
                 } else {
                     tvTimer.text = "00 : $it"
                 }
+                if(tvTimer.text.equals("00 : 00") ){
+                    tvTimer.visibility = View.GONE
+                    clRoot.isEnabled = false
+                    tvResult.visibility = View.VISIBLE
+                    tvResult.text = "Your\nresult\n${countResult}"
+                }
                 clRoot.setOnClickListener {
                     countResult++
                     Log.d("MyLog", countResult.toString())
-
-                    if (countLevel == 10) {
-                        val sharedPreferences =
-                            context?.getSharedPreferences("sharedSprint", Context.MODE_PRIVATE)
-                        val editor = sharedPreferences?.edit()
-                        editor.apply {
-                            this!!.putInt("resultSprint", countResult)
-                        }?.apply()
-                    } else if (countLevel == 20) {
-                        val sharedPreferences =
-                            context?.getSharedPreferences("sharedMiddle", Context.MODE_PRIVATE)
-                        val editor = sharedPreferences?.edit()
-                        editor.apply {
-                            this!!.putInt("resultMiddle", countResult)
-                        }?.apply()
-                    } else {
-                        val sharedPreferences =
-                            context?.getSharedPreferences("sharedLong", Context.MODE_PRIVATE)
-                        val editor = sharedPreferences?.edit()
-                        editor.apply {
-                            this!!.putInt("resultLong", countResult)
-                        }?.apply()
-                    }
-
                 }
+
+//                if (tvTimer.text.equals("00:00")) {
+//                    clRoot.isEnabled = false
+//                    tvTimer.visibility = View.GONE
+//                    tvResult.visibility = View.VISIBLE
+//                    tvResult.text = "Your\nresult\n${countResult}"
+//                    if(countLevel==10){
+//                        val prefsSprint = context?.getSharedPreferences("sharedSprintNew", Context.MODE_PRIVATE)
+//                        val resultSprintNew: Int? = prefsSprint?.getInt("resultSprintNew", 0)
+//                        if(countResult> resultSprintNew!!){
+//                            tvNewRecord.visibility = View.VISIBLE
+//                        }
+//                    }
+//                    if(countLevel==20) {
+//                        val prefsSprint =
+//                            context?.getSharedPreferences("sharedMiddleNew", Context.MODE_PRIVATE)
+//                        val resultMiddleNew: Int? = prefsSprint?.getInt("resultMiddleNew", 0)
+//                        if (countResult > resultMiddleNew!!) {
+//                            tvNewRecord.visibility = View.VISIBLE
+//                        }
+//                    }
+//                    if(countLevel==30){
+//                        val prefsSprint =
+//                            context?.getSharedPreferences("sharedLongNew", Context.MODE_PRIVATE)
+//                        val resultLongNew: Int? = prefsSprint?.getInt("resultLongNew", 0)
+//                        if (countResult > resultLongNew!!) {
+//                            tvNewRecord.visibility = View.VISIBLE
+//                        }
+//                    }
+//                }
 
             }, {
                 Log.d("MyLog", "Mistake")
 
             })
+    }
+    fun setShared(){
+//        if (countLevel == 10) {
+//            val sharedPreferences =
+//                context?.getSharedPreferences("sharedSprint", Context.MODE_PRIVATE)
+//            val editor = sharedPreferences?.edit()
+//            editor.apply {
+//                this!!.putInt("resultSprint", countResult)
+//            }?.apply()
+//        } else if (countLevel == 20) {
+//            val sharedPreferences =
+//                context?.getSharedPreferences("sharedMiddle", Context.MODE_PRIVATE)
+//            val editor = sharedPreferences?.edit()
+//            editor.apply {
+//                this!!.putInt("resultMiddle", countResult)
+//            }?.apply()
+//        } else {
+//            val sharedPreferences =
+//                context?.getSharedPreferences("sharedLong", Context.MODE_PRIVATE)
+//            val editor = sharedPreferences?.edit()
+//            editor.apply {
+//                this!!.putInt("resultLong", countResult)
+//            }?.apply()
+//        }
     }
 }
